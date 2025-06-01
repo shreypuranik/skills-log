6f188d4353ceae9c17ba5bbefbe0c6271a7a7d33
Shrey Puranik <shrey@shreypuranik.co.uk>
2025-06-01 13:29:54 +0100

    SL feat: Amending SQL, and setting up docker-compose.yaml

--- resource/sql/schema.sql
+++ resource/sql/schema.sql
@@ -16,6 +16,6 @@ CREATE TABLE member_skill_rating (
     skill_id INT NOT NULL,
     rating INT NOT NULL CHECK (rating BETWEEN 1 AND 5),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (member_id) REFERENCES Member(id),
-    FOREIGN KEY (skill_id) REFERENCES Skill(id)
+    FOREIGN KEY (member_id) REFERENCES member(id),
+    FOREIGN KEY (skill_id) REFERENCES skill(id)
 );
